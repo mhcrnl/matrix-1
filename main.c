7b61d97585ef16e46102fa2b2ae38beaf700fab1
Daniel Kappelle <daniel.kappelle@gmail.com>
2016-02-02 18:48:31 +0100

    Added comments

--- main.c
+++ main.c
@@ -4,23 +4,28 @@
 #include <stdlib.h>
 
 int main(int argc, char *args[]) {
+  /* Initialize some variables */
   struct winsize size;
   int len, i;
-  ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-  
-  printf("lines: %d\n", size.ws_row);
-  printf("cols: %d\n", size.ws_col);
 
+  /* Get window size */
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+
+  /* Calculate total length of string */
   len = size.ws_row * size.ws_col;
   char* filler = malloc(len+1);
 
+  /* Create string of proper length */
   for (i = 0; i < len; i++) {
     filler[i] = '#';
   }
 
+  /* Add terminating null character */
   filler[len+1] = '\0';
 
+  /* Print the string */
   printf("%s", filler);
 
   return 0;
 }
+
