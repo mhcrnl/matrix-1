9e8a5676411b81d42f4041661a1f24ebcab8fd18
Daniel Kappelle <daniel.kappelle@gmail.com>
2016-02-02 19:24:24 +0100

    Can now print entire screen and update

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+
+void delay(int millis);
 
 int main(int argc, char *args[]) {
   /* Initialize some variables */
@@ -14,28 +17,40 @@ int main(int argc, char *args[]) {
   /* Calculate total length of string */
   len = size.ws_row * size.ws_col;
   char* filler = malloc(len+1);
-  /* char* bla = malloc(len+1);*/
+  char* bla = malloc(len+1);
 
   /* Create string of proper length */
   for (i = 0; i < len; i++) {
     filler[i] = '#';
-    /* bla[i] = '.';*/
+    bla[i] = '.';
   }
 
   /* Add terminating null character */
   filler[len+1] = '\0';
-  /* bla[len+1] = '\0';*/
+  bla[len+1] = '\0';
 
   /* Print the string */
   while(1) {
-    printf("%s", filler);
-    usleep(500000);
     printf("\f");
-    /* printf("%s", bla);*/
-    /* usleep(500000);*/
-    /* printf("\f");*/
+    printf("%s", filler);
+    fflush(stdout);
+    delay(500);
+    printf("\f");
+    printf("%s", bla);
+    fflush(stdout);
+    delay(500);
   }
 
   return 0;
 }
 
+void delay(int millis) {
+  long pause;
+  clock_t now, then;
+
+  pause = millis*(CLOCKS_PER_SEC/1000);
+  now = then = clock();
+  while( (now-then) < pause ) {
+    now = clock();
+  }
+}
