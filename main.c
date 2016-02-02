9dd6fc419b9a544f376d5a0232290b5d51f4ecd7
Daniel Kappelle <daniel.kappelle@gmail.com>
2016-02-02 19:03:44 +0100

    temporarily stashing this here

--- main.c
+++ main.c
@@ -14,17 +14,27 @@ int main(int argc, char *args[]) {
   /* Calculate total length of string */
   len = size.ws_row * size.ws_col;
   char* filler = malloc(len+1);
+  /* char* bla = malloc(len+1);*/
 
   /* Create string of proper length */
   for (i = 0; i < len; i++) {
     filler[i] = '#';
+    /* bla[i] = '.';*/
   }
 
   /* Add terminating null character */
   filler[len+1] = '\0';
+  /* bla[len+1] = '\0';*/
 
   /* Print the string */
-  printf("%s", filler);
+  while(1) {
+    printf("%s", filler);
+    usleep(500000);
+    printf("\f");
+    /* printf("%s", bla);*/
+    /* usleep(500000);*/
+    /* printf("\f");*/
+  }
 
   return 0;
 }
