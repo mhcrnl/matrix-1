f9c4cce0204066b3d1145251d02e59e1c074c2c2
Daniel Kappelle <daniel.kappelle@gmail.com>
2016-02-03 19:09:22 +0100

    Created semi-clean main.c, will use 1 file for everything

--- main.c
+++ main.c
@@ -7,45 +7,10 @@
 #define KRED "\x1B[0m"
 
 void delay(int millis);
+void getWindow(int *row, int *col);
 
 int main(int argc, char *args[]) {
-  /* Initialize some variables */
-  struct winsize size;
-  int len, i;
 
-  /* Get window size */
-  ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-
-  /* Calculate total length of string */
-  len = size.ws_row * size.ws_col;
-  char* filler = malloc(len+1);
-  char* bla = malloc(len+1);
-
-  /* Create string of proper length */
-  for (i = 0; i < len; i++) {
-    filler[i] = '#';
-    bla[i] = '.';
-  }
-
-  /* Add terminating null character */
-  filler[len+1] = '\0';
-  bla[len+1] = '\0';
-
-  printf("%sreddddddd\n\n\n", KRED);
-
-  /* Print the string */
-  while(1) {
-    printf("\033[0;0H");
-    printf("\033[31m");
-    printf("%s", filler);
-    fflush(stdout);
-    delay(500);
-    printf("\033[0;0H");
-    printf("\033[34m");
-    printf("%s", bla);
-    fflush(stdout);
-    delay(500);
-  }
 
   return 0;
 }
@@ -59,4 +24,17 @@ void delay(int millis) {
   while( (now-then) < pause ) {
     now = clock();
   }
+}
+
+void getWindow(int *row, int *col) {
+  /* Initialize some variables */
+  struct winsize size;
+
+  /* Get window size */
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+
+  /* Stash them variables */
+  *row = size.ws_row;
+  *col = size.ws_col;
+
 }
